9c52621ab7698fcbd53e144dfe8f206019747c49
Diego Charrez <dcharrezt@unsa.edu.pe>
2020-05-06 16:28:15 -0500

    .gitignore is now working

--- docs/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Starting the community bonding",
-        "excerpt":"I am glad that I would be learning and working with the awesome community of Jderobot. I share their passion for developing robotics applications. firsts steps For my first issue, I have to set up a blog where I can write my progress. This repository is going to have code...","categories": ["GSoC"],
-        "tags": ["Jderobot"],
-        "url": "http://localhost:4000/gsoc/Starting-the-community-bonding/",
-        "teaser":null}]
